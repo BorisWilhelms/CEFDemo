bea9601ca878fefc8a20ab9d748032370dcb9f39
Boris Wilhelms <mail@wille-zone.de>
2018-03-26 12:08:55 +0200

    Refactoring

---
+++ CEFDemo/ApplicationBridge.cs
@@ -0,0 +1,39 @@
+﻿using CefSharp;
+using Newtonsoft.Json;
+using System;
+using System.Windows.Forms;
+
+namespace CEFDemo
+{
+    public class ApplicationBridge
+    {
+        private readonly IFrame _frame;
+        private readonly Action<ToDoItem> _onEdit;
+
+        public ApplicationBridge(IFrame frame, Action<ToDoItem> onEdit)
+        {
+            _frame = frame;
+            _onEdit = onEdit;
+        }
+
+        public void Edit(ToDoItem item) =>
+            _onEdit?.Invoke(item);
+
+        public bool Delete(ToDoItem item)
+        {
+            var result = MessageBox.Show(
+                $"Do you really want to delete the Item: \n {item.Text}",
+                "Delete item",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            return result == DialogResult.Yes;
+        }
+
+        public void UpdateItem(ToDoItem item)
+        {
+            var json = JsonConvert.SerializeObject(item);
+            _frame.ExecuteJavaScriptAsync($"(function(){{ updateItem({json}) }})();");
+        }
+    }
+}
--- CEFDemo/MainForm.cs
+++ CEFDemo/MainForm.cs
@@ -9,14 +9,14 @@ namespace CEFDemo
     public partial class MainForm : Form
     {
         private ChromiumWebBrowser _browser;
-        private string _currentId = null;
+        private ApplicationBridge _bridge;
+        private string _currentId;
 
         public MainForm() =>
             InitializeComponent();
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
             var url = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "HTML\\App.html");
             _browser = new ChromiumWebBrowser(url)
             {
@@ -26,31 +26,29 @@ namespace CEFDemo
             CefPanel.Controls.Add(_browser);
         }
 
-        private void MainForm_DoubleClick(object sender, EventArgs e) => _browser.ShowDevTools();
+        private void MainForm_DoubleClick(object sender, EventArgs e) =>
+            _browser.ShowDevTools();
 
         private void JavascriptObjectRepository_ResolveObject(object sender, CefSharp.Event.JavascriptBindingEventArgs e)
         {
             if (e.ObjectName == "bridge")
             {
-                var context = new ApplicationBridge
-                {
-                    OnEdit = (item) =>
-                    {
-                        _currentId = item.Id;
-                        Invoke((Action)(() => InputTextBox.Text = item.Text));
-                    }
-                };
-
-                e.ObjectRepository.Register("bridge", context, true, BindingOptions.DefaultBinder);
+                _bridge = new ApplicationBridge(_browser.GetMainFrame(), EditItem);
+                e.ObjectRepository.Register("bridge", _bridge, true, BindingOptions.DefaultBinder);
             }
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            var item = $"{{ Id: '{_currentId ?? Guid.NewGuid().ToString()}', Text: '{InputTextBox.Text}' }}";
-            _browser.GetMainFrame().ExecuteJavaScriptAsync($"(function(){{ updateItem({item}) }})();");
+            _bridge?.UpdateItem(new ToDoItem() { Id = _currentId ?? Guid.NewGuid().ToString(), Text = InputTextBox.Text });
             _currentId = null;
             InputTextBox.Text = String.Empty;
+        }
+
+        private void EditItem(ToDoItem item)
+        {
+            _currentId = item.Id;
+            Invoke((Action)(() => InputTextBox.Text = item.Text));
         }
     }
 }
