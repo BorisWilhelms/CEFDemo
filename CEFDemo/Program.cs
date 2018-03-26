4837a6a51065ac76a2187c5781fc101d49fde381
Boris Wilhelms <mail@wille-zone.de>
2018-03-26 09:03:35 +0200

    Initial Commit

--- CEFDemo/Program.cs
+++ CEFDemo/Program.cs
@@ -16,7 +16,7 @@ namespace CEFDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
